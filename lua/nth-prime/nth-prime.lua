350520b658e9fdfc6c8f4625e89576e9e2767d88
Jelle Van den Eynde <40927974+ArtOfBBQ@users.noreply.github.com>
2020-07-13 16:51:56 +0900

    Update nth-prime.lua

--- lua/nth-prime/nth-prime.lua
+++ lua/nth-prime/nth-prime.lua
@@ -1,33 +1,27 @@
-function is_prime(num, smaller_primes)
+local function is_prime(num, smaller_primes)
 
     local max_to_check = math.sqrt(num)
 
     local i = 1
     while (smaller_primes[i] <= max_to_check) do
-
         if num % smaller_primes[i] == 0 then return false end
         i = i + 1
-
     end
 
     return true
-
 end
 
-function get_next_prime(primes_seen)
+local function get_next_prime(primes_seen)
 
     if #primes_seen == 0 then return 2 end
 
-    candidate = primes_seen[#primes_seen] + 2
+    local candidate = primes_seen[#primes_seen] + 2
 
     while is_prime(candidate, primes_seen) == false do
-
         candidate = candidate + 2
-
     end
 
     return candidate
-
 end
 
 return function(n)
@@ -37,11 +31,8 @@ return function(n)
     local primes_seen = { 2, 3 }
 
     while (n > #primes_seen) do
-
         primes_seen[#primes_seen + 1] = get_next_prime(primes_seen)
-
     end
 
     return primes_seen[n]
-
 end
