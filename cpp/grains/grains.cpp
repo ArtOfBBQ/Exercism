d4a6da5833a47b7971ec3b5626e30852ad018b71
Jelle Van den Eynde <40927974+ArtOfBBQ@users.noreply.github.com>
2020-07-13 23:59:50 +0900

    grains with bitwise shift operator

--- cpp/grains/grains.cpp
+++ cpp/grains/grains.cpp
@@ -6,8 +6,18 @@ namespace grains {
 
     unsigned long long square(int square) {
     
-        return exp2(square - 1);
-
+        // The bitwise "shift left" operator
+        // *********************************
+        //
+        // this code gives the same result
+        // as math.pow(2ULL, square - 1)
+        //
+        // let's write the number 1 as binary, e.g. 0001
+        // 
+        // this will become 0010
+        // when it's "shifted left" 1x
+        // 0010 is 2, 0100 is 4, 1000 is 8, etc.
+        return 1ULL << (square - 1);
     }
 
     unsigned long long total() {
