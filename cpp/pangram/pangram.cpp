8c65f6120d9d15a97b4cebff989de02ed3be0874
Jelle Van den Eynde <40927974+ArtOfBBQ@users.noreply.github.com>
2020-07-17 11:33:52 +0900

    c++ pangram improvement

--- cpp/pangram/pangram.cpp
+++ cpp/pangram/pangram.cpp
@@ -2,22 +2,18 @@
 
 namespace pangram {
 
-    bool is_pangram(std::string input) {
+    bool is_pangram(const std::string& input) {
 
         if (input.size() == 0) { return false; }
 
-        // save each unseen letter in here,
-        // then check if we have all 26 in the alphabet    
-        std::unordered_set<char> alphabet = std::unordered_set<char>();
+        std::unordered_set<char> letters_seen;
          
-        for (unsigned long i = 0; i < input.size(); i++) {
-            if (isalpha(input[i]) &&
-                alphabet.find(tolower(input[i])) == alphabet.end()) {
-                
-                alphabet.insert(tolower(input[i]));
+        for (char current_char : input) {
+            if (isalpha(current_char)) {
+                letters_seen.insert(tolower(current_char));
             }
         }
          
-        return alphabet.size() == 26;
+        return letters_seen.size() == 26;
     }
 }
